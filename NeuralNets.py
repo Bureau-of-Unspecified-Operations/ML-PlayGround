f68e9a20359f757e14828dc4565ff5f0cf09f980
jecneps <jpspence@andrew.cmu.edu>
2018-04-19 10:15:54 -0400

    Mostly made backProp

--- NeuralNets.py
+++ NeuralNets.py
@@ -1,17 +1,66 @@
+import numpy as np
 
 
-class Net(Object):
-
-	neuron = Sigmoid() , neuron = Perceptron()
-
-	neuron.fire(net)
 
 
 
 class Layer(object):
 
-	def __init__(self, neuron, size):
-		self.nodes = n
+	def __init__(self, neuron, size, prevOut):
+		self.size = size
+		self.neuron = neuron
+		self.prevOut = prevOut
+		self.weights = self.initWeights(size,prevOut)
+		self.cachedOutput = None
+		self.cachedSigmas = None
+
+	def computeOutput(self):
+		out = list()
+		for w in self.weights:
+			net = np.dot(w,prevOut)
+			out.append(self.neuron.fire(net))
+		return np.array(out)
+
+	def derivativeAt(x): pass
+
+
+# destructively modifies net to update weights.
+def backpropagateSGD(data, step, net):
+	def downStreamError(sigmas,weights):
+		# assert len(sigmas) == len(wieghts)
+		errorArray = np.zeros(len(weights[0]))
+		for i, w in enumerate(weights):
+			errorArray = np.add(errorArray, sigmas[i] * w)
+		return errorArray
+
+
+	(ex, label) = data
+	net.cachedCompute(ex);
+
+
+	for layer in Layers:
+		localDerivative = layer.neuron.derivative(layer.cachedOutput)
+
+		if layer.type == Layer.OUTPUT:
+			error = np.subtract(label, layer.cachedOutput):
+			layer.cachedSigmas = -1 * np.multiply(error, localDerivative)
+
+		elif layer.type == Layer.HIDDEN:
+			error = downStreamError(layer.downLayer.cachedSigmas, layer.downLayer.weights)
+			layer.cachedSigmas = np.multiply(error, localDerivative)
+
+		elif layer.type == Layer.INPUT:
+			pass
+
+	## UPDATE WEIGHTS
+	for layer in Layers:
+		for w in layer.weights:
+			w = w + step * np.multiply(layer.upLayer.cachedOutput, layer.cachedSigmas)
+
+
+
+backpropagateSGD()
+
 
 
 
