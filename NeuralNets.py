13f22a8491fbb37deb1d7d41c385fb19a615efb3
jecneps <jpspence@andrew.cmu.edu>
2018-04-19 10:57:50 -0400

    Done making Types of Layer

--- NeuralNets.py
+++ NeuralNets.py
@@ -6,22 +6,51 @@ import numpy as np
 
 class Layer(object):
 
-	def __init__(self, neuron, size, prevOut):
-		self.size = size
+	def __init__(self, neuron, downLayer, upLayer, size, typein):
 		self.neuron = neuron
-		self.prevOut = prevOut
-		self.weights = self.initWeights(size,prevOut)
-		self.cachedOutput = None
-		self.cachedSigmas = None
+		self.upLayer = upLayer
+		self.downLayer = downLayer
+		self.weights = weights
+		self.type = type
+		self.cachedOutput = np.zeros(size)
+		self.cachedSigmas = np.zeros(size)
 
-	def computeOutput(self):
+	def compute(self):
 		out = list()
 		for w in self.weights:
-			net = np.dot(w,prevOut)
+			net = np.dot(w, self.upLayer.cachedOutput)
 			out.append(self.neuron.fire(net))
-		return np.array(out)
+		self.cachedOutput = np.array(out)
+		return self.cachedOutput
 
-	def derivativeAt(x): pass
+class HiddenLayer(Layer):
+
+class OutputLayer(Layer):
+	def __init__(self, neuron, upLayer, size):
+		super().__init__(neuron, None, upLayer, size, Layer.OUTPUT)
+
+class InputLayer(Layer):
+	def __init__(self, downLayer, size):
+		super().__init__(self, None, downLayer, None, size, Layer.INPUT)
+
+
+class NeuralNet(object):
+
+	def __init__(self):
+		self.inputLayer
+		self.outputLayer
+		self.layers
+
+
+	def compute(self, example):
+		self.inputLayer.cachedOutput = example
+		layer = self.inputLayer.downLayer
+		while(layer.type != Layer.OUTPUT):
+			layer.compute()
+			layer = layer.downLayer
+		return layer.compute
+
+
 
 
 # destructively modifies net to update weights.
@@ -33,29 +62,28 @@ def backpropagateSGD(data, step, net):
 			errorArray = np.add(errorArray, sigmas[i] * w)
 		return errorArray
 
-
 	(ex, label) = data
 	net.cachedCompute(ex);
+	layer = net.outputLayer
 
-
-	for layer in Layers:
+	while layer.type != Layer.INPUT:
 		localDerivative = layer.neuron.derivative(layer.cachedOutput)
-
+		error = np.zeros(len(localDerivative))  
 		if layer.type == Layer.OUTPUT:
 			error = np.subtract(label, layer.cachedOutput):
-			layer.cachedSigmas = -1 * np.multiply(error, localDerivative)
-
 		elif layer.type == Layer.HIDDEN:
 			error = downStreamError(layer.downLayer.cachedSigmas, layer.downLayer.weights)
-			layer.cachedSigmas = np.multiply(error, localDerivative)
+		layer.cachedSigmas = -1 * np.multiply(error, localDerivative)
+		layer = layer.upLayer
 
-		elif layer.type == Layer.INPUT:
-			pass
+
 
 	## UPDATE WEIGHTS
-	for layer in Layers:
+	layer = net.outputLayer
+	while layer.type != INPUT:
 		for w in layer.weights:
 			w = w + step * np.multiply(layer.upLayer.cachedOutput, layer.cachedSigmas)
+		layer = layer.upLayer
 
 
 
